a2d70b328ffcc52488fc83c36de449c826d9a4b1
IsabelCbernal <isabel.bernal@blankfactor.com>
2023-12-15 09:55:25 -0500

    First commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Contact Title",
+  description: "Contact Title",
+};
+
+export default function ContactPage() {
+  return <span className="text-7xl">Contact Page</span>;
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Pricing Title",
+  description: "Pricing Title",
+};
+
+export default function PrincingPage() {
+  return <span className="text-7xl">Pricing Page</span>;
+}
